7b75dc985a40b4d91cb558eef3104e06383a4e9b
Juan cruz Leone <juancruzleone@outlook.com>
2024-05-28 18:28:29 -0300

    Se modifico el diseño de la pagina 404

--- front/src/pages/404.jsx
+++ front/src/pages/404.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import Layout from "@/components/layout";
+import Footer from "@/components/Footer";
 
 const Custom404 = () => {
     return(
-        <>
-        </>
+        <Layout>
+            <section className='contenedor404'>
+                <h1 className='titulo404'>Código 404</h1>
+                <p className='texto404'>Página no encontrada</p>
+                <div className='contenedorImg404'>
+                    <Image src='/manguera.png' alt="Icono de manguera" width={100} height={100}/>
+                </div> 
+                <Link href="/" className='boton-404'>Ir al inicio</Link>
+            </section>
+            <Footer />
+        </Layout>
     )
 }
 
