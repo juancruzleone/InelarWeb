0d6c970f95c3dcd638e099134383de56bb18ff83
Juan cruz Leone <juancruzleone@outlook.com>
2024-06-06 13:58:12 -0300

    Se arreglo la funcion de crear y eliminar clientes en el panel

--- back/schemas/clientes.schema.js
+++ back/schemas/clientes.schema.js
@@ -2,13 +2,13 @@ import yup from 'yup';
 
 const clienteSchemaCreate = yup.object({
     name: yup.string().required(),
-    cathegory: yup.string().required(),
+    category: yup.string().required(),
     // Puedes añadir más validaciones según tus necesidades
 });
 
 const clienteSchemaPatch = yup.object({
     name: yup.string(),
-    cathegory: yup.string(),
+    category: yup.string(),
     // Añade más validaciones según tus necesidades
 });
 
