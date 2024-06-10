024b531b372330cc1becf81a6a06ef9a1e7afd00
Juan cruz Leone <juancruzleone@outlook.com>
2024-06-10 00:22:36 -0300

    Se agrego un buscador de productos y clientes en el panel admin

--- front/src/components/ListaProductos.jsx
+++ front/src/components/ListaProductos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Image from 'next/image'
+import Image from 'next/image';
 import Modal from "react-modal";
 import styles from "@/styles/Home.module.css";
 
@@ -8,24 +8,53 @@ Modal.setAppElement("#__next");
 
 const ListaProductos = () => {
   const [productos, setProductos] = useState([]);
+  const [categorias, setCategorias] = useState([]);
+  const [productosFiltrados, setProductosFiltrados] = useState([]);
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState(null);
+  const [modalCrear, setModalCrear] = useState(false);
   const [modalEditar, setModalEditar] = useState(false);
   const [modalEliminar, setModalEliminar] = useState(false);
-  const [modalCrear, setModalCrear] = useState(false);
   const [productoSeleccionado, setProductoSeleccionado] = useState(null);
-  const [nuevoProducto, setNuevoProducto] = useState({ name: "", description: "", price: "", categoria: "", imagen: "" });
+  const [nuevoProducto, setNuevoProducto] = useState({ name: "", categoria: "", description: "", price: "", imagen: "" });
+  const [busqueda, setBusqueda] = useState("");
 
   useEffect(() => {
+    const obtenerProductos = async () => {
+      try {
+        const response = await fetch("http://localhost:2023/api/productos");
+        const data = await response.json();
+        setProductos(data);
+        // Obtener las categorías únicas de los productos, excluyendo los productos sin categoría
+        const categoriasUnicas = Array.from(new Set(data.map((producto) => producto.categoria))).filter((categoria) => categoria !== undefined && categoria !== "");
+        setCategorias(categoriasUnicas.map((categoria) => categoria.charAt(0).toUpperCase() + categoria.slice(1)));
+      } catch (error) {
+        console.error("Error al obtener productos:", error);
+      }
+    };
     obtenerProductos();
   }, []);
 
-  const obtenerProductos = async () => {
-    try {
-      const response = await fetch("http://localhost:2023/api/productos");
-      const data = await response.json();
-      setProductos(data);
-    } catch (error) {
-      console.error("Error al obtener productos:", error);
+  useEffect(() => {
+    filtrarProductos();
+  }, [categoriaSeleccionada, productos, busqueda]);
+
+  const filtrarProductos = () => {
+    let productosFiltrados = productos;
+    if (categoriaSeleccionada) {
+      productosFiltrados = productosFiltrados.filter((producto) => producto.categoria === categoriaSeleccionada);
     }
+    if (busqueda) {
+      productosFiltrados = productosFiltrados.filter((producto) => producto.name.toLowerCase().includes(busqueda.toLowerCase()));
+    }
+    setProductosFiltrados(productosFiltrados);
+  };
+
+  const handleClickCategoria = (categoria) => {
+    setCategoriaSeleccionada(categoria);
+  };
+
+  const handleCrearProducto = () => {
+    setModalCrear(true);
   };
 
   const handleEditarProducto = (producto) => {
@@ -38,23 +67,38 @@ const ListaProductos = () => {
     setModalEliminar(true);
   };
 
-  const handleAbrirModalCrear = () => {
-    setNuevoProducto({ name: "", description: "", price: "", categoria: "", imagen: "" });
-    setModalCrear(true);
-  };
-
   const handleCerrarModal = () => {
+    setModalCrear(false);
     setModalEditar(false);
     setModalEliminar(false);
-    setModalCrear(false);
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     if (modalCrear) {
       setNuevoProducto({ ...nuevoProducto, [name]: value });
-    } else {
+    } else if (modalEditar) {
       setProductoSeleccionado({ ...productoSeleccionado, [name]: value });
+    }
+  };
+
+  const handleSubmitCrear = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:2023/api/productos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(nuevoProducto),
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+      const createdProducto = await response.json();
+      setProductos([...productos, createdProducto]);
+      handleCerrarModal();
+    } catch (error) {
+      console.error("Error al crear producto:", error);
     }
   };
 
@@ -62,10 +106,8 @@ const ListaProductos = () => {
     e.preventDefault();
     try {
       const response = await fetch(`http://localhost:2023/api/productos/${productoSeleccionado._id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(productoSeleccionado),
       });
       if (!response.ok) {
@@ -97,40 +139,46 @@ const ListaProductos = () => {
     }
   };
 
-  const handleSubmitCrear = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch("http://localhost:2023/api/productos", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(nuevoProducto),
-      });
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.error);
-      }
-      const createdProducto = await response.json();
-      setProductos([...productos, createdProducto]);
-      handleCerrarModal();
-    } catch (error) {
-      console.error("Error al crear producto:", error);
-    }
+  const handleBusqueda = (e) => {
+    setBusqueda(e.target.value);
   };
 
   return (
     <div>
       <div className={styles.contenedorPagina}>
         <h1 className={styles.tituloPaginasPanel}>Productos</h1>
-        <button onClick={handleAbrirModalCrear} className={styles.botonCrearModal}>Crear Producto</button>
+        <button onClick={handleCrearProducto} className={styles.botonCrearModal}>
+          Crear Producto
+        </button>
+        <input
+          type="text"
+          className={styles.buscadorPanel}
+          placeholder="Busca el nombre del producto"
+          value={busqueda}
+          onChange={handleBusqueda}
+        />
         <div className={styles.posicionSeccionProductos}>
-          <div className={styles.contenedorProductosPanel}>
-            {productos.map((producto, index) => (
+          <div className={styles.contenedorCategorias}>
+            {/* Lista de categorías como contenedores */}
+            {categorias.map((categoria, index) => (
+              <div
+                key={index}
+                className={`${styles.contenedorCategoria} ${categoria === categoriaSeleccionada ? styles.categoriaSeleccionada : ""}`}
+                onClick={() => handleClickCategoria(categoria)}
+              >
+                <p>{categoria}</p>
+              </div>
+            ))}
+          </div>
+          {/* Lista de productos filtrada por categoría */}
+          <div className={styles.contenedorClientes}>
+            {productosFiltrados.map((producto, index) => (
               <div key={index} className={styles.tarjetaProductoPanelProductos}>
                 <h3>{producto.name}</h3>
                 <div>
-                  <button onClick={() => handleEditarProducto(producto)}>Editar</button>
+                  <button onClick={() => handleEditarProducto(producto)} className={styles.botonEliminar}>
+                    <Image src="/eliminar.png" alt="Editar" width={10} height={10} />
+                  </button>
                   <button onClick={() => handleEliminarProducto(producto)} className={styles.botonEliminar}>
                     <Image src="/eliminar.png" alt="Eliminar" width={10} height={10} />
                   </button>
@@ -140,131 +188,55 @@ const ListaProductos = () => {
           </div>
         </div>
       </div>
-
+      {/* Modal Crear */}
+      <Modal isOpen={modalCrear} onRequestClose={handleCerrarModal} contentLabel="Crear Producto" className={styles.ModalPanel}>
+        <h2>Crear Producto</h2>
+        <form onSubmit={handleSubmitCrear} className={styles.formularioPanel}>
+          <label htmlFor="name">Nombre:</label>
+          <input type="text" id="name" name="name" value={nuevoProducto.name} onChange={handleChange} />
+          <label htmlFor="categoria">Categoría:</label>
+          <input type="text" id="categoria" name="categoria" value={nuevoProducto.categoria} onChange={handleChange} />
+          <label htmlFor="description">Descripción:</label>
+          <input type="text" id="description" name="description" value={nuevoProducto.description} onChange={handleChange} />
+          <label htmlFor="price">Precio:</label>
+          <input type="number" id="price" name="price" value={nuevoProducto.price} onChange={handleChange} />
+          <label htmlFor="imagen">Imagen:</label>
+          <input type="text" id="imagen" name="imagen" value={nuevoProducto.imagen} onChange={handleChange} />
+          <button type="submit">Crear</button>
+        </form>
+      </Modal>
       {/* Modal Editar */}
-      <Modal
-        isOpen={modalEditar}
-        onRequestClose={handleCerrarModal}
-        contentLabel="Editar Producto"
-        className={styles.ModalPanel}
-      >
+      <Modal isOpen={modalEditar} onRequestClose={handleCerrarModal} contentLabel="Editar Producto" className={styles.Modal}>
         <h2>Editar Producto</h2>
         {productoSeleccionado && (
-          <form onSubmit={handleSubmitEditar}>
+          <form onSubmit={handleSubmitEditar} className={styles.formularioPanel}>
             <label htmlFor="name">Nombre:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={productoSeleccionado.name}
-              onChange={handleChange}
-            />
-            <label htmlFor="description">Descripción:</label>
-            <input
-              type="text"
-              id="description"
-              name="description"
-              value={productoSeleccionado.description}
-              onChange={handleChange}
-            />
-            <label htmlFor="price">Precio:</label>
-            <input
-              type="number"
-              id="price"
-              name="price"
-              value={productoSeleccionado.price}
-              onChange={handleChange}
-            />
+            <input type="text" id="name" name="name" value={productoSeleccionado.name} onChange={handleChange} />
             <label htmlFor="categoria">Categoría:</label>
-            <input
-              type="text"
-              id="categoria"
-              name="categoria"
-              value={productoSeleccionado.categoria}
-              onChange={handleChange}
-            />
+            <input type="text" id="categoria" name="categoria" value={productoSeleccionado.categoria} onChange={handleChange} />
+            <label htmlFor="description">Descripción:</label>
+            <input type="text" id="description" name="description" value={productoSeleccionado.description} onChange={handleChange} />
+            <label htmlFor="price">Precio:</label>
+            <input type="number" id="price" name="price" value={productoSeleccionado.price} onChange={handleChange} />
             <label htmlFor="imagen">Imagen:</label>
-            <input
-              type="text"
-              id="imagen"
-              name="imagen"
-              value={productoSeleccionado.imagen}
-              onChange={handleChange}
-            />
+            <input type="text" id="imagen" name="imagen" value={productoSeleccionado.imagen} onChange={handleChange} />
             <button type="submit">Guardar</button>
           </form>
         )}
       </Modal>
-
       {/* Modal Eliminar */}
-      <Modal
-        isOpen={modalEliminar}
-        onRequestClose={handleCerrarModal}
-        contentLabel="Eliminar Producto"
-        className={styles.Modal}
-      >
+      <Modal isOpen={modalEliminar} onRequestClose={handleCerrarModal} contentLabel="Eliminar Producto" className={styles.Modal}>
         <h2>Eliminar Producto</h2>
-        <p>¿Estás seguro de eliminar el producto {productoSeleccionado?.name}?</p>
-        <button onClick={handleSubmitEliminar}>Eliminar</button>
-        <button onClick={handleCerrarModal}>Cancelar</button>
+        {productoSeleccionado && (
+          <div>
+            <p>¿Estás seguro de que deseas eliminar el producto {productoSeleccionado.name}?</p>
+            <button onClick={handleSubmitEliminar}>Eliminar</button>
+            <button onClick={handleCerrarModal}>Cancelar</button>
+          </div>
+        )}
       </Modal>
-      {/* Modal Crear */}
-<Modal
-  isOpen={modalCrear}
-  onRequestClose={handleCerrarModal}
-  contentLabel="Crear Producto"
-  className={styles.ModalPanel}
->
-  <h2>Crear Producto</h2>
-  <form onSubmit={handleSubmitCrear} className={styles.formularioPanel}>
-    <label htmlFor="name">Nombre:</label>
-    <input
-      type="text"
-      id="name"
-      name="name"
-      value={nuevoProducto.name}
-      onChange={handleChange}
-    />
-    <label htmlFor="description">Descripción:</label>
-    <input
-      type="text"
-      id="description"
-      name="description"
-      value={nuevoProducto.description}
-      onChange={handleChange}
-    />
-    <label htmlFor="price">Precio:</label>
-    <input
-      type="number"
-      id="price"
-      name="price"
-      value={nuevoProducto.price}
-      onChange={handleChange}
-    />
-    <label htmlFor="categoria">Categoría:</label>
-    <input
-      type="text"
-      id="categoria"
-      name="categoria"
-      value={nuevoProducto.categoria}
-      onChange={handleChange}
-    />
-    <label htmlFor="imagen">Imagen:</label>
-    <input
-      type="text"
-      id="imagen"
-      name="imagen"
-      value={nuevoProducto.imagen}
-      onChange={handleChange}
-    />
-    <button type="submit">Crear</button>
-  </form>
-</Modal>
-</div>
-);
+    </div>
+  );
 };
 
 export default ListaProductos;
-
-
-     
