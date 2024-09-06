a2147b20519690af9564764153a75f593a37f60d
Juan cruz Leone <juancruzleone@outlook.com>
2024-09-06 17:22:40 -0300

    Se modifico como se carga la tipografia

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="es">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
       <Head />
       <body>
         <Main />
