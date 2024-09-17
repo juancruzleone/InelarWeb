f92ef380769b7c96eafd3b3c6181b1029cb395f0
Juan cruz Leone <juancruzleone@outlook.com>
2024-09-17 02:19:39 -0300

    Se agrego el mail en el perfil de usuario

--- src/components/perfil/components/PerfilUsuario.jsx
+++ src/components/perfil/components/PerfilUsuario.jsx
@@ -4,8 +4,15 @@ const PerfilUsuario = ({ user, setShowEditModal }) => (
   <div className={styles.contenedorPerfilUsuario}>
     <div className={styles.contenidoPerfilUsuario}>
       <h1>Perfil de {user.userName}</h1>
-      <p className={styles.idUsuario}>{user._id}</p>
-      <button onClick={() => setShowEditModal(true)} className={styles.botonEditarPerfil}>Editar Perfil</button>
+      <p className={styles.idUsuario}>ID: {user._id}</p>
+
+      {user.role !== 'admin' && (
+        <p className={styles.emailUsuario}>Email: {user.email}</p>
+      )}
+
+      <button onClick={() => setShowEditModal(true)} className={styles.botonEditarPerfil}>
+        Editar Perfil
+      </button>
     </div>
   </div>
 );
