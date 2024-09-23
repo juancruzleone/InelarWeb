175c4f21e4467aaacc6ee3077048596e7fdebac0
Juan cruz Leone <juancruzleone@outlook.com>
2024-09-23 12:03:45 -0300

    Se agrego el uso del token en la sección servicios del panel admin

--- src/components/panel/ListaServicios/services/FetchListaServicios.jsx
+++ src/components/panel/ListaServicios/services/FetchListaServicios.jsx
@@ -1,9 +1,25 @@
 export async function fetchServices() {
   try {
-    const response = await fetch("https://inelarweb-back.onrender.com/api/servicios");
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    const userData = JSON.parse(localStorage.getItem('userData'));
+
+    
+    if (!userData || !userData.token) {
+      throw new Error('Token no encontrado en localStorage');
     }
+
+    
+    const response = await fetch("https://inelarweb-back.onrender.com/api/servicios", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${userData.token}`  
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('La respuesta de la red no fue satisfactoria');
+    }
+
     return await response.json();
   } catch (error) {
     console.error("Error fetching services:", error);
