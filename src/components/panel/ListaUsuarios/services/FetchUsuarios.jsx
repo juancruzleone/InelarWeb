cb1fe8fd623918a1efe6f8bb97093157ee3c8fd3
Juan cruz Leone <juancruzleone@outlook.com>
2024-09-23 12:00:47 -0300

    Se agrego el uso del token en algunas secciones del panel admin

--- src/components/panel/ListaUsuarios/services/FetchUsuarios.jsx
+++ src/components/panel/ListaUsuarios/services/FetchUsuarios.jsx
@@ -1,12 +1,27 @@
 export async function fetchAllUsers() {
-    try {
-      const response = await fetch('https://inelarweb-back.onrender.com/api/cuentas');
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return await response.json();
-    } catch (error) {
-      console.error("Error fetching users:", error);
-      throw error;
+  try {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData || !userData.token) {
+      throw new Error('Token no encontrado en localStorage');
     }
+
+    
+    const response = await fetch('https://inelarweb-back.onrender.com/api/cuentas', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${userData.token}`  
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error("La respuesta de la red no fue satisfactoria");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
   }
+}
