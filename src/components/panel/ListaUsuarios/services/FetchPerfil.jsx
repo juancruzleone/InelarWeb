bf3753fa1535283cb01f47f9d3fed484c9edb02b
Juan cruz Leone <juancruzleone@outlook.com>
2024-09-27 12:41:32 -0300

    Se agrego la obtencion del token al ver perfil de usurio

--- src/components/panel/ListaUsuarios/services/FetchPerfil.jsx
+++ src/components/panel/ListaUsuarios/services/FetchPerfil.jsx
@@ -1,9 +1,22 @@
 export async function fetchUserProfile(id) {
   try {
-    const response = await fetch(`https://inelarweb-back.onrender.com/api/cuentas/${id}`);
+    const token = localStorage.getItem('token');
+    
+    if (!token) {
+      throw new Error("No se encontró el token de autenticación");
+    }
+
+    const response = await fetch(`https://inelarweb-back.onrender.com/api/cuentas/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
+
     return await response.json();
   } catch (error) {
     console.error("Error fetching user profile:", error);
