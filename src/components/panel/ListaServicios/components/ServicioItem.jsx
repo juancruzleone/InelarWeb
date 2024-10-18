a629e68923ef01c47d11278a5e3144eeba3d179c
Juan cruz Leone <juancruzleone@outlook.com>
2024-10-18 02:52:33 -0300

    Se agrego la categoria en los servicios

--- src/components/panel/ListaServicios/components/ServicioItem.jsx
+++ src/components/panel/ListaServicios/components/ServicioItem.jsx
@@ -1,9 +1,11 @@
-import styles from "@/styles/ListaServicios.module.css";
+import styles from "@/styles/ListaServicios.module.css"
+import { capitalizeFirstLetter } from "@/components/panel/ListaServicios/utils/StringUtils.jsx"
 
 export default function ServicioItem({ service }) {
   return (
     <div className={styles.tarjetaProductoPanel}>
       <h3>Cliente: {service.nombre}</h3>
+      <p className={styles.categoriaServicio}> {capitalizeFirstLetter(service.category)}</p>
       <div className={styles.contenidoTarjetaProductoPanel}>
         <p><span>Email:</span> {service.email}</p>
         <p><span>Télefono:</span> {service.telefono}</p>
@@ -11,7 +13,8 @@ export default function ServicioItem({ service }) {
         <p><span>Dispositivo:</span> {service.dispositivo}</p>
         <p><span>Cantidad:</span> {service.cantidad}</p>
         <p><span>Fecha:</span> {service.fecha}</p>
+    
       </div>
     </div>
-  );
+  )
 }
