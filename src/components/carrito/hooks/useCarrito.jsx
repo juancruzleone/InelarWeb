31f8668ec67407f322b5c2fdfd64cd8303338472
Juan cruz Leone <juancruzleone@outlook.com>
2024-10-30 18:48:27 -0300

    Se soluciono un errro al eliminar productos del carrito

--- src/components/carrito/hooks/useCarrito.jsx
+++ src/components/carrito/hooks/useCarrito.jsx
@@ -1,71 +1,82 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { getSavedCart, getUserData, clearCart } from '@/components/carrito/utils/CartUtils';
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Cookies from 'js-cookie'
+import { getSavedCart, getUserData, clearCart } from '@/components/carrito/utils/CartUtils'
 
 const useCarrito = () => {
-  const router = useRouter();
-  const [cart, setCart] = useState([]);
-  const [userData, setUserData] = useState(null);
+  const router = useRouter()
+  const [cart, setCart] = useState([])
+  const [userData, setUserData] = useState(null)
   const [modalState, setModalState] = useState({
     isOpen: false,
     confirmOpen: false,
     deleteOpen: false,
     successOpen: false,
-  });
-  const [action, setAction] = useState(null);
-  const [selectedProductIndex, setSelectedProductIndex] = useState(null);
+  })
+  const [action, setAction] = useState(null)
+  const [selectedProductIndex, setSelectedProductIndex] = useState(null)
 
   useEffect(() => {
-    const savedCart = getSavedCart();
-    setCart(savedCart);
+    const savedCart = getSavedCart()
+    setCart(savedCart)
 
-    const userData = getUserData();
-    setUserData(userData);
+    const userData = getUserData()
+    setUserData(userData)
 
     if (Array.isArray(router.query.status) && router.query.status.includes('success')) {
-      clearCart();
-      setCart([]);
-      setModalState({ ...modalState, successOpen: true });
+      clearCart()
+      setCart([])
+      setModalState({ ...modalState, successOpen: true })
 
-      const cleanUrl = router.asPath.split('?')[0];
-      router.replace(cleanUrl, undefined, { shallow: true });
+      const cleanUrl = router.asPath.split('?')[0]
+      router.replace(cleanUrl, undefined, { shallow: true })
     }
-  }, [router.query.status]);
+  }, [router.query.status])
+
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      Cookies.set('carrito', JSON.stringify(cart))
+    }
+  }, [cart])
 
   const handleRemoveProduct = (index) => {
     if (cart[index].unidades > 1) {
-      const newCart = [...cart];
-      newCart[index].unidades -= 1;
-      setCart(newCart);
+      const newCart = [...cart]
+      newCart[index].unidades -= 1
+      setCart(newCart)
+      Cookies.set('carrito', JSON.stringify(newCart))
     } else {
-      setSelectedProductIndex(index);
-      setModalState({ ...modalState, deleteOpen: true });
+      setSelectedProductIndex(index)
+      setModalState({ ...modalState, deleteOpen: true })
     }
-  };
+  }
 
   const confirmRemoveProduct = () => {
-    const newCart = [...cart];
-    newCart.splice(selectedProductIndex, 1);
-    setCart(newCart);
-    setModalState({ ...modalState, deleteOpen: false });
-  };
+    const newCart = [...cart]
+    newCart.splice(selectedProductIndex, 1)
+    setCart(newCart)
+    Cookies.set('carrito', JSON.stringify(newCart))
+    setModalState({ ...modalState, deleteOpen: false })
+  }
 
   const handleIncreaseUnits = (index) => {
-    const newCart = [...cart];
-    newCart[index].unidades += 1;
-    setCart(newCart);
-  };
+    const newCart = [...cart]
+    newCart[index].unidades += 1
+    setCart(newCart)
+    Cookies.set('carrito', JSON.stringify(newCart))
+  }
 
   const handleEmptyCart = () => {
-    setAction('emptyCart');
-    setModalState({ ...modalState, confirmOpen: true });
-  };
+    setAction('emptyCart')
+    setModalState({ ...modalState, confirmOpen: true })
+  }
 
   const confirmEmptyCart = () => {
-    clearCart();
-    setCart([]);
-    setModalState({ ...modalState, confirmOpen: false });
-  };
+    clearCart()
+    setCart([])
+    setModalState({ ...modalState, confirmOpen: false })
+  }
 
   return {
     cart,
@@ -80,7 +91,7 @@ const useCarrito = () => {
     handleEmptyCart,
     confirmEmptyCart,
     setAction,
-  };
-};
+  }
+}
 
-export default useCarrito;
+export default useCarrito
