d5e655d076779397bdbde082d3e223c8f92009f2
Juan cruz Leone <juancruzleone@outlook.com>
2024-11-04 18:23:42 -0300

    Se modificaron los iconos del nav

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -7,12 +7,20 @@ export function ThemeToggle() {
   return (
     <button onClick={toggleTheme} className={styles.themeToggle} aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}>
       {theme === 'light' ? (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#F7931E" width="40" height="40">
-          <path d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#F7931E" fill="none" width="40" height="40">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
         </svg>
       ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#F7931E" width="40" height="40">
-          <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#F7931E" fill="none" width="40" height="40">
+          <circle cx="12" cy="12" r="5" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" />
+          <line x1="12" y1="1" x2="12" y2="3" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" />
+          <line x1="12" y1="21" x2="12" y2="23" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" />
+          <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" />
+          <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" />
+          <line x1="1" y1="12" x2="3" y2="12" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" />
+          <line x1="21" y1="12" x2="23" y2="12" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" />
+          <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" />
+          <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" />
         </svg>
       )}
     </button>
