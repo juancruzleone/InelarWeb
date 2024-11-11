1de4e2d21e46eb11f6b769730d39814614dc300e
Juan cruz Leone <juancruzleone@outlook.com>
2024-11-11 13:13:06 -0300

    Se soluciono un error en el form

--- src/pages/formulario/[id].jsx
+++ src/pages/formulario/[id].jsx
@@ -1,9 +1,9 @@
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
-import { getDeviceForm } from '@/components/formularios/services/FormularioService'
-import FormularioDispositivo from '@/components/formularios/components/FormularioDispositivo'
-import LoadingSpinner from '@/components/formularios/components/Cargando'
-import ErrorMessage from '@/components/formularios/components/Error'
+import { getDeviceForm } from '@/components/formularios/services/FormularioService.jsx'
+import FormularioDispositivo from '@/components/formularios/components/FormularioDispositivos.jsx'
+import LoadingSpinner from '@/components/formularios/components/Cargando.jsx'
+import ErrorMessage from '@/components/formularios/components/Error.jsx'
 import styles from '@/styles/ListaDispositivos.module.css'
 
 export default function PaginaFormularioDispositivo() {
