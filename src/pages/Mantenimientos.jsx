d0820ae6ddb320208536f8e87514542f11c153eb
Juan cruz Leone <juancruzleone@outlook.com>
2024-12-08 19:31:12 -0300

    Se modifico el diseño del servicio de mantenimiento

--- src/pages/Mantenimientos.jsx
+++ src/pages/Mantenimientos.jsx
@@ -20,12 +20,12 @@ const Mantenimiento = () => {
         <h1 className={styles.tituloServicio}>Mantenimientos</h1>
         <div className={styles.contenedorPosicionamientoContenidoServicio}>
           <div className={styles.contenedorTextoServicio}>
-            <p className={styles.textoServicios}>Inelar presta servicios de mantenimientos a todos los clientes que lo deseen. Contamos con equipo de última tecnología para llevar a cabo los mantenimientos y un equipo de técnicos especializados para su realización.</p>
-            <p className={styles.textoServicios}>Además, ofrecemos a nuestros clientes una aplicación para llevar un registro de los mantenimientos realizados y arreglar posibles fallas de los dispositivos en caso de que no puedan contactar con un técnico. Así, mantenemos un control exhaustivo de los dispositivos y garantizamos su buen estado funcionamiento.</p>
+            <p className={styles.textoServicios}>En <span>Inelar</span> prestamos servicios de mantenimientos a todos los clientes que lo deseen. <span>Contamos con equipo de última tecnología para llevar a cabo los mantenimientos y un equipo de técnicos especializados para su realización.</span></p>
+            <p className={styles.textoServicios}>Además, <span>ofrecemos a nuestros clientes una aplicación</span> para llevar un registro de los mantenimientos realizados y arreglar posibles fallas de los dispositivos en caso de que no puedan contactar con un técnico. Así, mantenemos un control exhaustivo de los dispositivos y garantizamos su buen estado funcionamiento.</p>
             <div className={styles.contenedorControlMantenimiento}>
               <h2 className={styles.subtituloMantenimiento}>Control de mantenimientos</h2>
-              <p className={styles.textoServicios}>Realizamos los mantenimientos de forma periódica, siguiendo un calendario establecido y adaptado a las necesidades de cada dispositivo. Una vez se realiza el mantenimiento, se registra en nuestra aplicación y se informa al cliente del estado del dispositivo.</p>
-              <p className={styles.textoServicios}>Además, realizamos un seguimiento de los dispositivos y sus elementos de forma individual, de modo que podamos anticipar posibles problemas y evitar averías mayores en el futuro.</p>
+              <p className={styles.textoServicios}><span>Realizamos los mantenimientos de forma periódica</span>, siguiendo un calendario establecido y adaptado a las necesidades de cada dispositivo. Una vez se realiza el mantenimiento, se registra en nuestra aplicación y se informa al cliente del estado del dispositivo.</p>
+              <p className={styles.textoServicios}>Además, <span>realizamos un seguimiento de los dispositivos y sus elementos de forma individual</span>, de modo que podamos anticipar posibles problemas y evitar averías mayores en el futuro.</p>
             </div>
           </div>
           <Image
